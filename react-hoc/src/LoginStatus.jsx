99c989b0a81e5a8d9689af602f259f44c8b471d7
huangbozhi <1044213015@qq.com>
2019-06-24 15:33:55 +0800

    update

---
+++ react-hoc/src/LoginStatus.jsx
@@ -0,0 +1,13 @@
+import React, {component} from 'react'
+class LoginStatus extends React.Component {
+  render() {
+    // if(!login) {
+    //   return null;
+    // }
+    return (
+      <button>已经登录</button>
+    );
+  }
+}
+LoginStatus.display = 'LoginStatus'
+export default LoginStatus;
