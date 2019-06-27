f7984971e53d9f9d0d209665dad7ff718fe49560
huangbozhi <1044213015@qq.com>
2019-06-27 10:03:35 +0800

    todolists

---
+++ react-redux-todolist/src/todo/TodoList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+class TodoList extends Component {
+  state = {  }
+  render() { 
+    const { todos, toggleTo } = this.props;
+    return (
+      <div>
+        {
+          todos.map((todo, i) =>{
+            return (
+              <li key={i}
+              onClick={() => {
+                toggleTo(todo.id)
+              }}
+              style={{
+                textDecoration: todo.completed ? 'line-through' : ''
+              }}
+              >{todo.text}</li>
+            )
+          })
+        }
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  console.log('渲染 todos 的时候type:', state.filterType)
+  return {
+    todos: state.todos
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    toggleTo: id => dispatch({
+      type: 'TOGGLE_TODO',
+      id
+    })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
