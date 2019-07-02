4cbe0ece45123e35ddc70f7cc0644a0296cd754b
huangbozhi <1044213015@qq.com>
2019-07-02 10:15:19 +0800

    hoc

--- react-hoc/src/App.js
+++ react-hoc/src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import LoginStatus from './LoginStatus';
 import ShopCart from './ShopCart';
+import ExampleMobx from './exampleMobx/index.jsx'
 import WithLogin from './WithLogin';
+import examplebase from './examplebase/index'
+import ExampleDeBounce from './ExampleDeBounce/index'
 import './App.css';
 
 // const WithLoginStatus = WithLogin(LoginStatus);
@@ -14,6 +17,8 @@ function App() {
       <ShopCart />
       {/* <WithLoginStatus />
       <WithShopCart a="1" b="2"/> */}
+      <ExampleMobx />
+      <ExampleDeBounce />
     </>
   )
 }
